27c285a21f1223342b8648b957e1f21c4a8c144f
sindhusri <sindhusri@LAPTOP-8HLVMP5T>
2022-06-29 06:20:52 +0530

    first

---
+++ GithubDemo/src/test/java/automationFrameworks/SummaTry.java
@@ -0,0 +1,14 @@
+package automationFrameworks;
+
+
+
+public class SummaTry {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sindhusri\\Desktop\\automation\\Selenium\\chromedriver_win32 (2)\\chromedriver.exe");
+	    
+	    
+
+	}
+
+}
